ef653038dcb45eeb5c823daa660ca16decb92adc
VSODUM <VSODUM@VSODUM-L480.us.oracle.com>
2021-06-23 02:11:09 +0530

    ipfile added

--- ip_scp.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-for eachip in 'cat ipfile';do
-scp target/sparkjava.war root@eachip:/opt/tomcat/webapps/
-done
--- test.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-for eachip in ipfile;do
-curl http://eachip:8080/sparkjava/hello
-done
